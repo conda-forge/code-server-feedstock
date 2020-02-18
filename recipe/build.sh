cc60a776ba4cb06405196b5eddddbd8182b94736
Uwe L. Korn <uwe.korn@quantco.com>
2020-02-18 21:16:08 +0100

    2.1698-vsc1.41.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 set -x
 
 VERSION=${PKG_VERSION//_/-}
-export NODE_BINARY_PATH=${PREFIX}/bin/node
-file ${NODE_BINARY_PATH}
 
 vscodeVersion=$(python -c "print('${VERSION}'.split('-vsc')[1])")
 codeServerVersion=$(python -c "print('${VERSION}'.split('-vsc')[0])")
