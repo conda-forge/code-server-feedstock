612f1acaf4ad07cbfff420a84934661a51bdbe00
Uwe L. Korn <uwelk@xhochy.com>
2020-02-20 12:12:27 +0100

    Use nbin

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,8 @@ VERSION=${PKG_VERSION//_/-}
 vscodeVersion=$(python -c "print('${VERSION}'.split('-vsc')[1])")
 codeServerVersion=$(python -c "print('${VERSION}'.split('-vsc')[0])")
 
+export NODE_BINARY_PATH=${PREFIX}/bin/node-nbin
+
 yarn
 MINIFY=true yarn build "${vscodeVersion}" "${codeServerVersion}"
 yarn binary "${vscodeVersion}" "${codeServerVersion}"
