6b6c10d425f22e44170aaaa05d227ffbf8c48799
Uwe L. Korn <uwe.korn@quantco.com>
2020-03-10 08:10:51 +0100

    code-server 3.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,22 @@
 set -euo pipefail
 set -x
 
-VERSION=${PKG_VERSION//_/-}
-
-vscodeVersion=$(python -c "print('${VERSION}'.split('-vsc')[1])")
-codeServerVersion=$(python -c "print('${VERSION}'.split('-vsc')[0])")
-
-export NODE_BINARY_PATH=${PREFIX}/bin/node-nbin
+# export NODE_BINARY_PATH=${PREFIX}/bin/node-nbin
 
 yarn
-MINIFY=true yarn build "${vscodeVersion}" "${codeServerVersion}"
-yarn binary "${vscodeVersion}" "${codeServerVersion}"
-mkdir -p ${PREFIX}/bin/
-mv binaries/code-server${VERSION}-*-x86_64 ${PREFIX}/bin/code-server
+
+pushd lib/vscode
+patch -p1 <../../ci/vscode.patch
+yarn
+npm rebuild
+popd
+
+yarn build
+mkdir -p ${PREFIX}/share/code-server
+mv build/* ${PREFIX}/share/code-server
+mkdir -p ${PREFIX}/bin
+cat <<'EOF' >${PREFIX}/bin/code-server
+#!/bin/bash
+node ${CONDA_PREFIX}/share/code-server/out/node/entry.js $*
+EOF
+chmod +x ${PREFIX}/bin/code-server
