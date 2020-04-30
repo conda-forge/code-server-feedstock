6c1ded90c63ff27147cbf97447eadd7fef50b1a6
Uwe L. Korn <uwe.korn@quantco.com>
2020-04-30 18:05:52 +0200

    Binary repack of 3.2.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
-VERSION=${PKG_VERSION//_/-}
-
-vscodeVersion=$(python -c "print('${VERSION}'.split('-vsc')[1])")
-codeServerVersion=$(python -c "print('${VERSION}'.split('-vsc')[0])")
-
-export NODE_BINARY_PATH=${PREFIX}/bin/node-nbin
-
-yarn
-MINIFY=true yarn build "${vscodeVersion}" "${codeServerVersion}"
-yarn binary "${vscodeVersion}" "${codeServerVersion}"
-mkdir -p ${PREFIX}/bin/
-mv binaries/code-server${VERSION}-*-x86_64 ${PREFIX}/bin/code-server
+mkdir $PREFIX/share
+cp -r code-server $PREFIX/share/
+mkdir -p ${PREFIX}/bin
+cat <<'EOF' >${PREFIX}/bin/code-server
+#!/bin/bash
+node ${CONDA_PREFIX}/share/code-server/out/node/entry.js $*
+EOF
+chmod +x ${PREFIX}/bin/code-server
