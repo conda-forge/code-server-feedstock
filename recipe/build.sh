7acf46f59671edb71155e09710b4ed13e80f6df7
Ben Mares <15216687+maresb@users.noreply.github.com>
2020-05-22 10:58:05 +0200

    Use bundled Node.js
    I think this should be more reliable than using whatever version happens to be available from Conda.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cp -r code-server $PREFIX/share/
 mkdir -p ${PREFIX}/bin
 cat <<'EOF' >${PREFIX}/bin/code-server
 #!/bin/bash
-node ${CONDA_PREFIX}/share/code-server/out/node/entry.js $*
+${CONDA_PREFIX}/share/code-server/lib/node ${CONDA_PREFIX}/share/code-server/out/node/entry.js $*
 EOF
 chmod +x ${PREFIX}/bin/code-server
 
