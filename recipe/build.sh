1eaa0b2fb6d7f086ede9bfc851473e7aa219dead
Uwe L. Korn <uwe.korn@quantco.com>
2020-06-03 09:28:45 +0200

    Remove unused bundled files

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,5 +15,12 @@ chmod +x ${PREFIX}/bin/code-server
 find ${PREFIX}/share/code-server -name '*.map' -delete
 rm -rf \
   ${PREFIX}/share/code-server/node \
+  ${PREFIX}/share/code-server/lib/node \
+  ${PREFIX}/share/code-server/lib/lib* \
+  ${PREFIX}/share/code-server/lib/vscode/node_modules/vscode-sqlite3/build/Release/obj* \
+  ${PREFIX}/share/code-server/lib/vscode/node_modules/vscode-sqlite3/build/Release/sqlite3.a \
+  ${PREFIX}/share/code-server/lib/vscode/node_modules/vscode-sqlite3/deps \
   ${PREFIX}/share/code-server/lib/vscode/node_modules/.cache \
-  ${PREFIX}/share/code-server/lib/vscode/out/vs/workbench/*.map
+  ${PREFIX}/share/code-server/lib/vscode/out/vs/workbench/*.map \
+  ${PREFIX}/share/code-server/lib/vscode/node_modules/@coder/requirefs/coverage
+find ${PREFIX}/share/code-server/ -name obj.target | xargs rm -r
